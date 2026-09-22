fa39a048fc91902286da4ef5dff7fc38cc3410a1
Filo37 <sanabriadavid504@gmail.com>
2026-09-22 16:24:25 -0300

    Delete scrip.js

--- scrip.js
+++
@@ -1,9 +0,0 @@
-botonTraducir = document.getElementById('bTraducir');
-
-
-botonTraducir. addEventListener ('click',
-    function() {
-        titulo = document.getElementById('titulo');
-        titulo. textContent = "Register form";
-    }
-) ;
