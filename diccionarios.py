d25c2c4d5e2998f18b7e0b8b723456311771fa0c
Filo37 <sanabriadavid504@gmail.com>
2026-09-22 15:16:38 -0300

    Add files via upload

---
+++ diccionarios.py
@@ -0,0 +1,5 @@
+productos = {"manzana": 5600, "mandarina": 12000, "kiwi": 25000}
+
+
+print ("Precio de la manzana en Gs. ", productos ["manzana"])
+print ("Precios", productos)
---
+++ scrip.js
@@ -0,0 +1,9 @@
+botonTraducir = document.getElementById('bTraducir');
+
+
+botonTraducir. addEventListener ('click',
+    function() {
+        titulo = document.getElementById('titulo');
+        titulo. textContent = "Register form";
+    }
+) ;
